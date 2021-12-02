ef258e673fb50261c8f3f189658eddfff7e09b6d
janice <janice.ivena@ti.ukdw.ac.id>
2021-12-03 04:00:36 +0700

    Soal 4

--- 4_A_71210812.py
+++ 4_A_71210812.py
@@ -1,7 +1,7 @@
 angka = int(input("Input : "))
 a = 2
-print("Output : ")
 
+print("Output : ")
 for tinggi in range(1,1+angka):
     for baris in range(1,2*angka):
         if tinggi + baris == angka+ 1 or baris - tinggi == angka - 1:
