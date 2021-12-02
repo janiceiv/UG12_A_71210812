60793bb62ef4b83fb364dbb11f155396c2896411
janice <janice.ivena@ti.ukdw.ac.id>
2021-12-03 03:44:15 +0700

    soal1

---
+++ 1_A_71210812.py
@@ -0,0 +1,11 @@
+angka = list(input("Masukkan deret angka : ").split())
+
+for i in (angka):
+    if (angka %2 != 0):
+        (- angka)
+    else:
+        (+ angka)
+
+sum(angka)
+print(sum)
+    
